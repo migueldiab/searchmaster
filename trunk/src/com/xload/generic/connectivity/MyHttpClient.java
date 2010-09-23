2937674273185c9919bd42f6124617a8188363d9
migueldiab <migueldiab@43f39e98-2ee1-11df-a735-6ff41f51f3aa>
2010-09-23 14:22:06 +0000

    Optional Proxy

--- trunk/src/com/xload/generic/connectivity/MyHttpClient.java
+++ trunk/src/com/xload/generic/connectivity/MyHttpClient.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.Credentials;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.params.AuthPolicy;
 import org.apache.http.conn.params.ConnRoutePNames;
@@ -24,23 +25,23 @@ public class MyHttpClient {
   public static DefaultHttpClient getClient() {
       DefaultHttpClient httpclient = new DefaultHttpClient();
 
-      //Credentials credentials = new  UsernamePasswordCredentials("sg0894180", "edcvfr4");
-      String  proxy_url      = AppProperties.getProperty(AppProperties.PROXY_URL);
-      Integer proxy_port     = Integer.parseInt(AppProperties.getProperty(AppProperties.PROXY_PORT));
-      String  proxy_user     = AppProperties.getProperty(AppProperties.PROXY_USER);
-      String  proxy_password = AppProperties.getProperty(AppProperties.PROXY_PASSWORD);
-      
-      httpclient.getCredentialsProvider().setCredentials(
-                new AuthScope(proxy_url, proxy_port),
-                new UsernamePasswordCredentials(proxy_user, proxy_password));
+      String  proxy_enabled      = AppProperties.getProperty(AppProperties.PROXY_URL);
+      if (proxy_enabled.equals("YES")) {
+          String  proxy_url      = AppProperties.getProperty(AppProperties.PROXY_URL);
+          Integer proxy_port     = Integer.parseInt(AppProperties.getProperty(AppProperties.PROXY_PORT));
+          String  proxy_user     = AppProperties.getProperty(AppProperties.PROXY_USER);
+          String  proxy_password = AppProperties.getProperty(AppProperties.PROXY_PASSWORD);
+          //Credentials credentials = new  UsernamePasswordCredentials(proxy_user, proxy_password);
+          httpclient.getCredentialsProvider().setCredentials(
+                    new AuthScope(proxy_url, proxy_port),
+                    new UsernamePasswordCredentials(proxy_user, proxy_password));
 
-      HttpHost proxy = new HttpHost(proxy_url, proxy_port);
-
-      List authPrefs = new ArrayList();
-        authPrefs.add(AuthPolicy.NTLM);
-
-      httpclient.getParams().setParameter(AuthPolicy.NTLM, authPrefs);
-      httpclient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxy);
+          HttpHost proxy = new HttpHost(proxy_url, proxy_port);
+          List<String> authPrefs = new ArrayList<String>();
+          authPrefs.add(AuthPolicy.NTLM);
+          httpclient.getParams().setParameter(AuthPolicy.NTLM, authPrefs);
+          httpclient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxy);
+      }
 
       return httpclient;
 
